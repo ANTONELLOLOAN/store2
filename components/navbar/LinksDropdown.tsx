b435b6b43f54879f4c404c448e9382b33462f48b
Loan <antonello.dominici@gmail.com>
2024-12-02 03:41:19 +0100

    Add Auth Provider

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -9,6 +9,9 @@ import { LuAlignLeft } from 'react-icons/lu';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import { links } from '@/utils/links';
+import UserIcon from './UserIcon';
+import SignOutLink from './SignOutLink';
+import { SignInButton, SignUpButton, SignedIn, SignedOut } from '@clerk/nextjs';
 
 function LinksDropdown() {
   return (
@@ -18,18 +21,40 @@ function LinksDropdown() {
           <LuAlignLeft className='w-6 h-6' />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='w-40' align='start' sideOffset={10}>
-        {links.map((link) => {
-          return (
-            <DropdownMenuItem key={link.href}>
-              <Link href={link.href} className='capitalize w-full'>
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+      <DropdownMenuContent className='w-48' align='start' sideOffset={10}>
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className='capitalize w-full'>
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <UserIcon />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
-export default LinksDropdown
+export default LinksDropdown;
